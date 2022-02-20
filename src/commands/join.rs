2ecc5894a3a4ed07ab4a6a834e841bd66d198824
Ryoga Saito <contact@proelbtn.com>
2022-02-20 21:46:57 +0900

    ロールのキャッシュを残す

--- src/commands/join.rs
+++ src/commands/join.rs
@@ -3,6 +3,8 @@ use crate::*;
 
 use serenity::model::prelude::*;
 use std::collections::HashMap;
+use serenity::futures::lock::Mutex;
+use serenity::http::CacheHttp;
 
 pub struct JoinCommand<Repository>
 where
@@ -11,6 +13,7 @@ where
     repository: Repository,
     guild_id: GuildId,
     mapping: HashMap<String, String>,
+    cache: Mutex<Option<HashMap<String, Role>>>,
 }
 
 impl<Repository> JoinCommand<Repository>
@@ -26,6 +29,37 @@ where
             repository,
             guild_id,
             mapping,
+            cache: Mutex::new(None),
+        }
+    }
+
+    #[tracing::instrument(skip(self, http))]
+    async fn find_role(&self, http: &serenity::http::Http, role_name: String) -> Result<Role> {
+        let guild_id = self.guild_id;
+
+        let mut guard = self.cache.lock().await;
+
+        match *guard {
+            Some(ref cache) => {
+                tracing::debug!("cache found");
+                Ok(cache.get(&role_name)
+                    .ok_or(SystemError::NoSuchRole(role_name))?
+                    .clone())
+            },
+            None => {
+                tracing::debug!("cache not found, fetching");
+                let roles_mapping: HashMap<_, _> = self.repository.find_all(http, guild_id).await?
+                    .into_iter()
+                    .map(|r| (r.name.clone(), r))
+                    .collect();
+
+                let role = roles_mapping.get(&role_name)
+                    .ok_or(SystemError::NoSuchRole(role_name))?.clone();
+
+                *guard = Some(roles_mapping);
+
+                Ok(role)
+            }
         }
     }
 
@@ -43,24 +77,20 @@ where
         let role_name = self
             .mapping
             .get(&invitation_code)
-            .ok_or(UserError::InvalidInvitationCode(invitation_code))?;
+            .ok_or(UserError::InvalidInvitationCode(invitation_code))?
+            .clone();
 
-        let _guild_id = self.guild_id;
         let user_id = ctx.command.user.id;
 
-        // 入るべきRoleの取得
-        // TODO: ロール名から毎回検索をかけずに、初回にRoleIdを解決する
-        let target_roles = self
-            .repository
-            .find_by_name(http, guild_id, role_name.clone())
-            .await?;
-        let target_role = target_roles
-            .first()
-            .ok_or(SystemError::NoSuchRole(role_name.into()))?;
+        let target_role = self.find_role(&ctx.context.http, role_name).await?;
 
         self.repository
             .grant(http, guild_id, user_id, target_role.id)
             .await?;
+
+        InteractionHelper::defer_respond(http, command, "チームに参加しました。").await;
+
+        tracing::debug!("deleting old roles");
         let granted_roles = self
             .repository
             .find_by_user(http, guild_id, user_id)
@@ -75,7 +105,6 @@ where
         }
 
         tracing::info!("granted");
-        InteractionHelper::defer_respond(http, command, "チームに参加しました。").await;
 
         Ok(())
     }
