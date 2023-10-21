0ef300a18cfcc4baab7540514480568ab5046044
Ryoga Saito <ryoga.saito@linecorp.com>
2023-10-22 01:21:01 +0900

    ファイルを一部削除した

--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -1,13 +1,5 @@
-mod channels;
 mod interactions;
 mod recreate;
-mod roles;
-mod threads;
-mod users;
 
-pub use channels::*;
 pub use interactions::*;
 pub use recreate::*;
-pub use roles::*;
-pub use threads::*;
-pub use users::*;
